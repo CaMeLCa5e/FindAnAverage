4be4addef251682b6ce60e07b143455085bb5dd7
CaMeLCa5e <jm273606@gmail.com>
2014-10-26 14:45:04 -0400

    updates
    (10.25.14) file processed but isalnum() caused inconsistencies because
    a '.' is non-alpha-num.  This took all float numbers out inadvertently.
    
    new file(10.26.14) uses CSV for file reading

--- find_an_average10.25.14.py
+++ find_an_average10.25.14.py
@@ -2,14 +2,15 @@
 """
 This is an application built to import a data file, average and return a value
 """
-import numpy
+import csv
+# import numpy
 
-def read_file(file_name):
-	with open(file_name, 'r') as f:
-		f.readline()
-		f.readline()
-		num = f.readline().split('|')[-1] 
-		print num
+# def read_file(file_name):
+# 	with open(file_name, 'r') as f:
+# 		# f.readline()
+# 		# f.readline()
+# 		num = f.readline().split('|')[-1] 
+# 		# print num
 
 
 
@@ -22,48 +23,68 @@ def read_file(file_name):
 # 				if i in line >= 0:
 # 					yield average.(total)
 # 				else:
-# 					return 
+# # 					return 
+# data ='data.txt'
+
+# incomes = [int(line.split("|")[-1].strip())
+
+#             for line in open(data,'r').readlines()[2:]
+
+#             if line.split("|")[-1].strip().isalnum()]
+
+# avg_income = sum(incomes) / len(incomes)
+
+# print avg_income
+
+
+def process():
+	with open('data.txt', 'r') as f:
+		f.readline()
+		f.readline()
+		column = []
+		for row in csv.reader(f, delimiter='|', skipinitialspace=True):
+			data = row[-1]
+			try:
+				data = float(data)
+			except ValueError as err:
+				print(err)
+				print(data)
+			else:
+				if data != -999999999:
+					column.append(data)
+	return sum(column)
 
 
 
-def consume():
-	with open(data, 'r') as f:
-	#display a running average 
-	running_sum = 0
-	data_items_seen = 0
 
-	while True:
-		for i line in enumerate(f.readlines()[2:]):
-			if i in line >=0:
-		yield = data
-		data_items_seen += len(data)
-		running_sum += sum(data)
-		print ('The average is {}'.format(running_sum / float(data_items_seen)))
+	# while True:
+	# 	for i, line in enumerate(f.readlines()[2:]):
+	# 		if i in line >=0:
+	# 			yield = data
+	# 	data_items_seen += len(data)
+	# 	running_sum += sum(data)
+	# 	print ('{}'.format(running_sum / float(data_items_seen)))
 	#data = f.read()
-	f.close()
-	#return data
+	# f.close()
+	
 
 
 
-def split_data(x):
-	data_count = {}
-	data_list = x.split("|")
+# def split_data(x):
+# 	data_count = {}
+# 	data_list = x.split("|")
 
-	for word in data_list:
-		# if word in data_count:
-		# 	data_count[word] +=1 
-		# else:
-		# 	data_count[word] = 1
-		print word
-	return data_count	
+# 	for word in data_list:
+# 		# if word in data_count:
+# 		# 	data_count[word] +=1 
+# 		# else:
+# 		# 	data_count[word] = 1
+# 		print word
+# 	return data_count	
 
 def main():
-	read_file("data.txt")
-	#data_dictionary = split_data(data)
-# 	print data_dictionary
-	# sorted_data = sorted([(value,key) for (key,value) in data_dictionary.items()])
-	# sorted_data.reverse()
-	# print sorted_data[0:9]
+
+	print process()
 
 if __name__ == '__main__':
 	main()
---
+++ find_an_average10.26.14.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python 
+"""
+This is an application built to import a data file, average and return a specific value.
+"""
+import csv
+
+def process():
+	with open('data.txt', 'r') as f:
+		
+		#skip the first two lines
+		f.readline()			
+		f.readline()
+
+		column = []
+		
+		#split by pipes, and take out whitespace
+		for row in csv.reader(f, delimiter='|', skipinitialspace=True):   
+			data = row[-1]
+			try:
+				data = float(data)
+
+			#Notify if data error
+			except ValueError as err: 
+				print(err)
+			
+			else:
+				if data != -999999999:
+					column.append(data)
+
+	return sum(column)
+
+def main():
+	print process()
+
+if __name__ == '__main__':
+	main()
+	
+
